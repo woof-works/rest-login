faa7fdad024495924e40e7662cfb10057053a19f
Tim <stcain@gmail.com>
2016-11-07 02:32:09 +0800

    New: User model that represents a user that is allowed to login

---
+++ src/main/java/woofworks/model/User.java
@@ -0,0 +1,26 @@
+package woofworks.model;
+
+/**
+ * Created by Tim on 07-Nov-16.
+ */
+public class User {
+    private Long id;
+
+    private String login;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
