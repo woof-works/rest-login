51a76dbce3f682d1621e3d6928ff04efd1babd29
Tim <stcain@gmail.com>
2016-11-07 02:32:30 +0800

    New: Sprint boot runner

---
+++ src/main/java/woofworks/Run.java
@@ -0,0 +1,16 @@
+package woofworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for running the spring boot app
+ *
+ * Created by Tim on 07-Nov-16.
+ */
+@SpringBootApplication
+public class Run {
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);
+    }
+}
