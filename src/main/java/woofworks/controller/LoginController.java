c99735705a3c8a4cb271c500188d52dbe9d2b6bb
Tim <stcain@gmail.com>
2016-11-12 15:46:35 +0800

    New: REST endpoint for logging in

--- src/main/java/woofworks/controller/LoginController.java
+++ src/main/java/woofworks/controller/LoginController.java
@@ -1,6 +1,11 @@
 package woofworks.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import woofworks.dto.LoginRequestDTO;
+import woofworks.dto.LoginResponseDTO;
+import woofworks.service.UserService;
 
 /**
  * Created by Tim on 11-Nov-16.
@@ -8,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public LoginResponseDTO login(@Validated @RequestBody LoginRequestDTO request) {
+        return userService.logUserIn(request);
+    }
 }
