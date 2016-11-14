b550406ec9d09095d83b86842028c82c64486d0e
Tim <stcain@gmail.com>
2016-11-14 21:50:02 +0800

    New: Setter / getter annotations

--- src/main/java/woofworks/dto/LoginResponseDTO.java
+++ src/main/java/woofworks/dto/LoginResponseDTO.java
@@ -1,8 +1,13 @@
 package woofworks.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Tim on 2016-11-12.
  */
+@Getter
+@Setter
 public class LoginResponseDTO {
 
     public String token;
